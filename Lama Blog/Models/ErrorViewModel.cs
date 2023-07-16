c33574735ce4c9ceddef25dbecb3740fdc3bd700
trivuong3000 <trivuong3000@gmail.com>
2023-07-16 18:31:54 +0700

    first commit

---
+++ Lama Blog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lama_Blog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
